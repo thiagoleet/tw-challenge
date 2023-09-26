9ea09bea0689e85653ddc6d5a72a536b6a498314
Thiago Leite <thiagofmleite@gmail.com>
2023-09-26 12:29:02 -0300

    Initial Code

---
+++ src/main/java/me/thiagoleite/twchallenge/TwChallengeApplication.java
@@ -0,0 +1,13 @@
+package me.thiagoleite.twchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/thiagoleite/twchallenge/TwChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.thiagoleite.twchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
