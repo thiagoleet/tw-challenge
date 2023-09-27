d53852c4fb71655e6fa301ab05bb81ffa60b15b1
Thiago Leite <thiagofmleite@gmail.com>
2023-09-27 16:59:49 -0300

    Fix OrderItem update

--- src/main/java/me/thiagoleite/twchallenge/services/OrderItemService.java
+++ src/main/java/me/thiagoleite/twchallenge/services/OrderItemService.java
@@ -3,15 +3,20 @@ package me.thiagoleite.twchallenge.services;
 import me.thiagoleite.twchallenge.model.entities.Order;
 import me.thiagoleite.twchallenge.model.entities.OrderItem;
 import me.thiagoleite.twchallenge.model.repositories.OrderItemRepository;
+import me.thiagoleite.twchallenge.model.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Service
 public class OrderItemService implements ApiService<OrderItem> {
     @Autowired
     private OrderItemRepository repository;
+
+    @Autowired
+    private OrderRepository orderRepository;
 
     public Iterable<OrderItem> findAll() {
         return repository.findAll();
@@ -30,8 +35,14 @@ public class OrderItemService implements ApiService<OrderItem> {
     public OrderItem update(Long id, OrderItem updatedData) {
         Optional<OrderItem> existingOrderItem = repository.findById(id);
         if (existingOrderItem.isPresent()) {
-            updatedData.setId(id);
-            return repository.save(updatedData);
+            Optional<Order> order = orderRepository.findById(updatedData.getOrder().getId());
+            if (order.isPresent()) {
+                updatedData.setOrder(order.get());
+                updatedData.setId(id);
+                return repository.save(updatedData);
+            } else {
+                throw new IllegalArgumentException("Order not found");
+            }
         } else {
             throw new IllegalArgumentException("Order Item not found with id: " + id);
         }
